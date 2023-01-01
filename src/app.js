c022f22886a6298567cd8cafd9f77a69ecf10f3b
Aida <a.mosayebi@outlook.com>
2023-01-01 13:26:11 +0330

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,20 @@ function displayTemprature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let city = "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemprature);
+  axios.get(apiUrl).then(displayTemprature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
